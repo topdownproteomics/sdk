99950c6b3b5c107df06437befccc1a84d53efca5
stefanks <solntsev@wisc.edu>
2018-01-09 16:10:07 -0600

    init

---
+++ LibraryProject1/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace TestLibNamespace
+{
+    public class Class1
+    {
+        #region Public Methods
+
+        public static int Return1()
+        {
+            return 1;
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ TestProject1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class Class1
+    {
+        #region Public Methods
+
+        [Test]
+        public static void TestFunction1()
+        {
+            Assert.AreEqual(1, TestLibNamespace.Class1.Return1());
+        }
+
+        #endregion Public Methods
+    }
+}
