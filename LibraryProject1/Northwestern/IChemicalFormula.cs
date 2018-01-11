d7e69adac8a0121d23f20df1afdb188de94306a9
Ryan Fellers <rfellers@gmail.com>
2018-01-11 16:07:13 -0600

    Northwestern initial commitment (#6)
    * loads NU initial contribution: Mercury7 and MSNumpress
    
    * adds a small addition to CloneAndShift() doc tag

---
+++ LibraryProject1/Northwestern/IChemicalFormula.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace TestLibNamespace.Northwestern
+{
+    /// <summary>
+    /// A collection of different elements in various quantities.
+    /// </summary>
+    public interface IChemicalFormula : IDualMass
+    {
+        /// <summary>
+        /// Gets the quantity for the specified element.
+        /// </summary>
+        int this[IElement element] { get; }
+
+        /// <summary>
+        /// Gets the element counts.
+        /// </summary>
+        IEnumerable<IElementCount> ElementCounts { get; }
+
+        /// <summary>
+        /// Determines if this formula equals another.
+        /// </summary>
+        /// <param name="other">The other.</param>
+        /// <returns></returns>
+        bool Equals(IChemicalFormula other);
+
+        /// <summary>
+        /// Determines whether [contains] [the specified element].
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <returns>
+        ///   <c>true</c> if [contains] [the specified element]; otherwise, <c>false</c>.
+        /// </returns>
+        bool Contains(IElement element);
+
+        /// <summary>
+        /// Adds the specified formula.
+        /// </summary>
+        /// <param name="formula">The formula.</param>
+        /// <returns></returns>
+        IChemicalFormula Add(IChemicalFormula formula);
+
+        /// <summary>
+        /// Subtracts the specified formula.
+        /// </summary>
+        /// <param name="formula">The formula.</param>
+        /// <returns></returns>
+        IChemicalFormula Subtract(IChemicalFormula formula);
+
+        /// <summary>
+        /// Multiplies the formula by the specified multiplier.
+        /// </summary>
+        /// <param name="multiplier">The multiplier.</param>
+        /// <returns></returns>
+        IChemicalFormula Multiply(int multiplier);
+    }
+}
---
+++ LibraryProject1/Northwestern/IMzIntensityData.cs
@@ -0,0 +1,35 @@
+﻿namespace TestLibNamespace.Northwestern
+{
+    /// <summary>
+    /// Represents simple spectral data that contains only m/z and intensity.
+    /// </summary>
+    public interface IMzIntensityData
+    {
+        /// <summary>
+        /// Gets the first m/z.
+        /// </summary>
+        double FirstMz { get; }
+
+        /// <summary>
+        /// Gets the last m/z.
+        /// </summary>
+        double LastMz { get; }
+
+        /// <summary>
+        /// Gets the m/z array.
+        /// </summary>
+        /// <returns></returns>
+        double[] GetMz();
+
+        /// <summary>
+        /// Gets the intensity array.
+        /// </summary>
+        /// <returns></returns>
+        double[] GetIntensity();
+
+        /// <summary>
+        /// Gets the length.
+        /// </summary>
+        int Length { get; }
+    }
+}
