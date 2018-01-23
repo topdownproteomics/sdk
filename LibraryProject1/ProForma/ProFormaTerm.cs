75ab7c1daf3534b3ef7431d03bb06df86beb1243
Ryan Fellers <rfellers@gmail.com>
2018-01-23 16:14:32 -0600

    add a basic ProForma parser with a handful of unit tests

---
+++ LibraryProject1/ProForma/ProFormaTerm.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace TestLibNamespace.ProForma
+{
+    /// <summary>
+    /// Represents a ProForma string in memory.
+    /// </summary>
+    public class ProFormaTerm
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProFormaTerm"/> class.
+        /// </summary>
+        /// <param name="sequence">The sequence.</param>
+        /// <param name="tags">The tags.</param>
+        public ProFormaTerm(string sequence, ICollection<ProFormaTag> tags)
+        {
+            Sequence = sequence;
+            Tags = tags;
+        }
+
+        /// <summary>
+        /// Gets the amino acid sequence.
+        /// </summary>
+        public string Sequence { get; }
+
+        /// <summary>
+        /// Gets the tags.
+        /// </summary>
+        public ICollection<ProFormaTag> Tags { get; }
+    }
+}
---
+++ TestProject1/ProFormaParserTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using TestLibNamespace.ProForma;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class ProFormaParserTests
+    {
+        public static ProFormaParser _parser = new ProFormaParser();
+
+        [Test]
+        public void InvalidProFormaStrings()
+        {
+            Assert.Throws<ArgumentNullException>(() => _parser.ParseString(null));
+            Assert.Throws<ArgumentNullException>(() => _parser.ParseString(string.Empty));
+        }
+
+        [Test]
+        public void NoModifications()
+        {
+            const string proFormaString = "PROTEOFORM";
+            var term = _parser.ParseString(proFormaString);
+
+            Assert.AreEqual(proFormaString, term.Sequence);
+            Assert.IsNull(term.Tags);
+        }
+
+        [Test]
+        public void SimpleTag()
+        {
+            const string proFormaString = "PRO[info:test]TEOFORM";
+            var term = _parser.ParseString(proFormaString);
+
+            Assert.AreEqual("PROTEOFORM", term.Sequence);
+            Assert.IsNotNull(term.Tags);
+            Assert.AreEqual(1, term.Tags.Count);
+            Assert.AreEqual(2, term.Tags.Single().Index);
+            Assert.AreEqual(1, term.Tags.Single().Descriptors.Count);
+            Assert.AreEqual("info", term.Tags.Single().Descriptors.Single().Key);
+            Assert.AreEqual("test", term.Tags.Single().Descriptors.Single().Value);
+        }
+    }
+}
