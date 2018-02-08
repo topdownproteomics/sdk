1905c0cf816abc6887e14b2d3fd5d6b663f89e8d
Anthony <anthony.cesnik@gmail.com>
2018-02-08 01:28:08 -0600

    Add support for single value descriptors.

--- LibraryProject1/ProForma/ProFormaDescriptor.cs
+++ LibraryProject1/ProForma/ProFormaDescriptor.cs
@@ -1,10 +1,15 @@
-﻿namespace TestLibNamespace.ProForma
+﻿using System;
+
+namespace TestLibNamespace.ProForma
 {
     /// <summary>
     /// Member of the tag. Could be a key-value pair, or a keyless entry.
     /// </summary>
     public class ProFormaDescriptor
     {
+
+        #region Constructors
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ProFormaDescriptor"/> class.
         /// </summary>
@@ -12,18 +17,53 @@
         /// <param name="value">The value.</param>
         public ProFormaDescriptor(string key, string value)
         {
-            this.Key = key;
+            if (Enum.TryParse(key, true, out ProFormaKey parsedKey))
+                this.Key = parsedKey;
+            else
+                throw new ProFormaParseException("The key " + key + " is not supported.");
             this.Value = value;
         }
 
         /// <summary>
+        /// Initializes a descriptor without value only
+        /// </summary>
+        /// <param name="value"></param>
+        public ProFormaDescriptor(string value)
+        {
+            this.Key = ProFormaKey.MOD;
+            this.Value = value;
+        }
+
+        #endregion Constructors
+
+        #region Public Properties
+
+        /// <summary>
         /// Gets the key.
         /// </summary>
-        public string Key { get; }
+        public ProFormaKey Key { get; }
 
         /// <summary>
         /// Gets the value.
         /// </summary>
         public string Value { get; }
+
+        #endregion Public Properties
+
+        #region Constants
+
+        /// <summary>
+        /// Possible keys for a ProFormaDescriptor
+        /// </summary>
+        public enum ProFormaKey
+        {
+            MOD,
+            MASS,
+            FORMULA,
+            INFO
+        }
+
+        #endregion Constants
+
     }
 }
