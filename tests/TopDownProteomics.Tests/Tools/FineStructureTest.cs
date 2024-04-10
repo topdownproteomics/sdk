d22319d851b6d94aa10ac2a2d5a29b025456dd57
Ryan Fellers <rfellers@gmail.com>
2024-04-10 15:04:51 -0500

    adds a unit test for fine structure isotopic generator

---
+++ tests/TopDownProteomics.Tests/Tools/FineStructureTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using TopDownProteomics.Chemistry;
+using TopDownProteomics.Tools;
+
+namespace TopDownProteomics.Tests.Tools;
+
+[TestFixture]
+public class FineStructureTest
+{
+    private readonly IElementProvider _elementProvider = new MockElementProvider();
+
+    [Test]
+    public void BasicTest()
+    {
+        var generator = new FineStructureIsotopicGenerator();
+
+        var result = generator.GenerateIsotopicDistribution(ChemicalFormula.ParseString("C6H12O6".AsSpan(), _elementProvider));
+
+        var mz = result.Masses;
+        var abun = result.Intensities;
+
+        Assert.AreEqual(180.0634, mz[0], 0.0001);
+        Assert.AreEqual(0.9226, abun[0], 0.0001);
+    }
+}
